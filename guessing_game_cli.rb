7df9651b19e8f30c3315811cba42a0990f914b8c
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-06-28 17:25:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   puts "Hello, Guess a number between 1 and 6."
   num = gets.chomp
  
-  if num == comp_num 
+  if num == comp_num.to_s
     puts "You guessed the correct number!"
 
   elsif num == "exit"
